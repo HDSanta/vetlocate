b3c874312333f007253a6cc1ecb967f95be0ff1d
Spar10SHS <140642943+Spar10SHS@users.noreply.github.com>
2026-03-15 14:15:31 -0700

    Force Netlify rebuild

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BenefitsLookup from './pages/BenefitsLookup';
 import VAFacilities from './pages/VAFacilities';
 import LocationCompare from './pages/LocationCompare';
+import PropertyTaxLookup from "./pages/PropertyTaxLookup";
 import Profile from './pages/Profile';
 import './App.css';
 
@@ -12,11 +13,12 @@ function App() {
       <div className="app">
 <nav className="navbar">
   <div style={{ display: 'flex', alignItems: 'center' }}>
-    <div className="nav-brand">⭐ VetLocate</div>
+    <Link to="/" className="nav-brand" style={{ textDecoration: 'none', color: 'inherit' }}>⭐ VetLocate</Link>
     <div className="nav-links">
-      <Link to="/">Benefits Lookup</Link>
+      <Link to="/">Lookup Benefits</Link>
       <Link to="/facilities">VA Facilities</Link>
       <Link to="/compare">Compare Locations</Link>
+      <Link to="/property-tax">Property Tax</Link>
     </div>
   </div>
   <div className="nav-right">
@@ -28,6 +30,7 @@ function App() {
             <Route path="/" element={<BenefitsLookup />} />
             <Route path="/facilities" element={<VAFacilities />} />
             <Route path="/compare" element={<LocationCompare />} />
+              <Route path="/property-tax" element={<PropertyTaxLookup />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
         </main>
